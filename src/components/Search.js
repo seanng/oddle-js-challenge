82fd86e2f476d4fdbcfec5a9f575ac0bb62ec844
Sean <shonum@gmail.com>
2016-12-24 13:25:15 +0800

    Complete searchbar functionality

--- src/components/Search.js
+++ src/components/Search.js
@@ -3,21 +3,33 @@ import React, { Component } from 'react';
 class Search extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      input: ''
+    };
   }
 
   render() {
     return (
       <div className="searchbar input-group input-group-lg">
-        <input type="text" onSubmit={this.handleSubmit.bind(this)} className="form-control" placeholder="Search by Github ID" aria-describedby="sizing-addon1" />
-        <span className="input-group-btn">
-          <button className="btn btn-default" type="button">Go!</button>
-        </span>
+        <form onSubmit={this.handleSubmit.bind(this)}>
+          <input onChange={this.handleTextChange.bind(this)} type="text" className="form-control" placeholder="Search by Github ID" aria-describedby="sizing-addon1" />
+          <span className="input-group-btn">
+            <button className="btn btn-default" type="button" onClick={this.handleSubmit.bind(this)}>Go!</button>
+          </span>
+        </form>
       </div>
     );
   }
 
+  handleTextChange(e) {
+    e.preventDefault();
+    this.setState({input: e.target.value})
+  }
+
   handleSubmit(e) {
-    this.props.submitQuery(e.target.value)
+    e.preventDefault();
+    console.log('hi', this.state.input)
+    this.props.submitQuery(this.state.input)
   }
 
 }
