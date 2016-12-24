9e8da06421212279db644dbea1e9a729b498245a
Sean <shonum@gmail.com>
2016-12-24 17:02:55 +0800

    Add image and github URL to the card

--- src/components/Result.js
+++ src/components/Result.js
@@ -1,10 +1,15 @@
-import React, { Component } from 'react';
+import React from 'react';
 import '../styles/Result.css';
 
-const Result = (props) => {
+const Result = ({result}) => {
   return (
-    <div className="result" >
-
+    <div className="result">
+      <div className="w3-card-2">
+        <img src={result.avatar_url} alt="" className="avatar" />
+        <div className="w3-container w3-center">
+          <p><a href={result.html_url}>{result.login}</a></p>
+        </div>
+      </div>
     </div>
   )
 }
