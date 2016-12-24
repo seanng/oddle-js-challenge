ba48ca9e11df255506edbba8f302eb2a8dafb46b
Sean <shonum@gmail.com>
2016-12-24 16:54:07 +0800

    Replace input state with input reference

--- src/components/Search.js
+++ src/components/Search.js
@@ -3,16 +3,13 @@ import React, { Component } from 'react';
 class Search extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      input: ''
-    };
   }
 
   render() {
     return (
       <div className="searchbar input-group input-group-lg">
         <form onSubmit={this.handleSubmit.bind(this)}>
-          <input onChange={this.handleTextChange.bind(this)} type="text" className="form-control" placeholder="Search by Github ID" aria-describedby="sizing-addon1" />
+          <input ref="input" type="text" className="form-control" placeholder="Search by Github ID" aria-describedby="sizing-addon1" />
           <span className="input-group-btn">
             <button className="btn btn-default" type="button" onClick={this.handleSubmit.bind(this)}>Go!</button>
           </span>
@@ -21,15 +18,9 @@ class Search extends Component {
     );
   }
 
-  handleTextChange(e) {
-    e.preventDefault();
-    this.setState({input: e.target.value})
-  }
-
   handleSubmit(e) {
     e.preventDefault();
-    console.log('hi', this.state.input)
-    this.props.submitQuery(this.state.input)
+    this.props.submitQuery(this.refs.input.value)
   }
 
 }
